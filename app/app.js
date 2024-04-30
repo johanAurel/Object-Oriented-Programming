44eeff3ae3afb2b359167356a1ff70c04539b9c2
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-04-30 17:01:56 +0100

    feat: trying to host app on webpage

--- app/app.js
+++ app/app.js
@@ -1,17 +1,28 @@
-const {
-    trainersName , changeName, showcasePokemon, chooseFirstPokemonT1, chooseFirstPokemonT2, choosePokemon, freeWill
-} = require('./gameTrial');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import express from 'express';
+import path from 'path';
 
-const express = require('express');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath1 = path.join(__dirname, './public');
+const publicDirectoryPath2 = path.join(__dirname, '../model/public');
 
 
+app.use(express.static(publicDirectoryPath1));
+app.use(express.static(publicDirectoryPath2));
+app.get('/1', (req, res) => {
+    res.sendFile('index.html', { root: publicDirectoryPath1 });
+});
+app.get('/2', (req, res) => {
+    res.sendFile('index.html', { root: publicDirectoryPath2 });
+});
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
 
-
- app.get('/api/teams', freeWill);
- 
-app.listen(9090, (err) => {
-  if (err){console.log(err)}
-  else {console.log('server listening on port 9090...')}
-})
